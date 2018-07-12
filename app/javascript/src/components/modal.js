69606dfe6cfbdcaa27de30e8cda3214aa27dddd1
peytonchance <peytonchance@gmail.com>
2018-07-12 12:19:36 -0400

    Refactors modal scripts as module.

---
+++ app/javascript/src/components/modal.js
@@ -0,0 +1,42 @@
+export default class Modal {
+  constructor(el) {
+    this.el = el
+    this.setVars()
+    this.setUpListeners()
+  }
+
+  setVars() {
+    this.bodyMain = document.querySelector('.body__main')
+    this.allTargets = Array.from(document.querySelectorAll('.form'))
+    this.targetModal = document.querySelector('.' + this.el.dataset.modal)
+  }
+
+  setUpListeners() {
+    this.el.addEventListener('click', this.toggleModal.bind(this))
+  }
+
+  toggleModal() {
+    if (this.targetModal.classList.contains('form--hidden')) {
+      this.allTargets.forEach((target) => {
+        target.classList.add('form--hidden')
+        this.bodyMain.classList.add('body__main--muted')
+      })
+      this.targetModal.classList.remove('form--hidden')
+    } else {
+      this.targetModal.classList.add('form--hidden')
+      this.targetModal.classList.add('form--no-blur')
+      this.bodyMain.classList.remove('body__main--muted')
+    }
+  }
+}
+
+
+//  this.allTargets.forEach((target) => {
+// //
+// //     })
+// //      currentModal.classList.remove('form--hidden')
+// //      currentModal.classList.add('form--no-blur')
+// //   } else {
+// //
+// //
+// //   }
