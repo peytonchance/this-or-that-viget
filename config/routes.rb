9ddf52df4b76eaa2758750c6d9a7ad0aeb795973
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-07-18 15:34:58 -0600

    Added follows controller (issue #20 and #28)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :polls do
     resource :comments, only: [:create]
+    resource :follows, only: [:create, :destroy]
   end
   
   resource :profiles, only: [:show]
