326e56149bbaed73b37ed76a86f0620cd9c643dd
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-07-20 15:31:03 -0600

    Added before_action filter to make sure token is correct

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,8 +1,11 @@
 class PasswordsController < Devise::PasswordsController
+  before_action :verify_user_reset?, only: :edit
+  
   def create
     @user = User.send_reset_password_instructions(password_params)
 
     if successfully_sent?(@user)
+      binding.pry
       render json: {
         "status": "success",
         "message": "Reset Password Link Sent to Email"
@@ -16,7 +19,6 @@ class PasswordsController < Devise::PasswordsController
   end
   
   def edit
-    binding.pry
     @user = User.new
     set_minimum_password_length
     @user.reset_password_token = params[:reset_password_token]
@@ -26,4 +28,12 @@ class PasswordsController < Devise::PasswordsController
   def password_params
     params.require(:user).permit(:email)
   end
+  
+  def verify_user_reset?
+    User.with_reset_password_token(params[:reset_password_token]).present?
+  end
+  
+  def after_resetting_password_path_for(resource)
+      redirect_to root_path
+    end
 end
