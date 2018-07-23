f92efec961c22f9b5f7e300e4ef5ba50ceb48415
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-07-23 14:40:34 -0600

    Added update action and refactored some methods

--- app/controllers/option_bs_controller.rb
+++ app/controllers/option_bs_controller.rb
@@ -3,21 +3,37 @@ class OptionBsController < ApplicationController
     @poll = Poll.find(params[:poll_id])
     @vote = @poll.votes.new(user: current_user, option: 1)
     if @vote.save
-      binding.pry
-      render json: {
-        "status": "success",
-        "poll": @poll.id,
-        "optionA": @poll.fraction_of_votes(0),
-        "optionB": @poll.fraction_of_votes(1)
-        }, status: :accepted
+      render json: vote_success_json, status: :accepted
     else
-      render json: {
-        "status": "error",
-        "message": "Please try again"
-        }, status: :unprocessable_entity
+      render json: vote_error_json, status: :unprocessable_entity
     end
   end
 
   def update
+    @poll = Poll.find(params[:poll_id])
+    @vote = @poll.votes.find_by(user_id: current_user.id)
+    if @vote.update(option: 1)
+      render json: vote_success_json, status: :accepted
+    else
+      render json: vote_error_json, status: :unprocessable_entity
+    end
+  end
+  
+  
+  private 
+  def vote_error_json
+    @hash =  {
+        "status": "error",
+        "message": "Please try again"
+        }
+  end
+  
+  def vote_success_json
+    @hash = {
+        "status": "success",
+        "poll": @poll.id,
+        "optionA": @poll.fraction_of_votes(0),
+        "optionB": @poll.fraction_of_votes(1)
+      }
   end
 end
