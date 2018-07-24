6936e68b9dd680b283982848c63d586214fe8ba4
peytonchance <peytonchance@gmail.com>
2018-07-24 11:47:32 -0400

    Minor JS cleanup.

--- app/assets/javascripts/polls/poll_follows.js
+++ app/assets/javascripts/polls/poll_follows.js
@@ -1,31 +1,27 @@
-$(document).ready(function() {
-   var $polls = $(".follow-poll")
+$(document).ready(() => {
+   let $polls = $(".follow-poll")
    $polls.bind('ajax:success', function(evt, data, status, xhr) {
-      updateFollow(evt, data, status, xhr);
+      updateFollow(evt, data, status, xhr)
    }).bind('ajax:error', function(evt, xhr, status, error){
       handleFollowError(evt, xhr, status, error)
    });
 });
 
-function updateFollow(evt, data, status, xhr) {
-   response = evt["detail"][0]
-   pollId = response["pollId"]
-   pollFollowElement = document.getElementById("poll-follow-" + pollId)
-   var pollFollowIcon = document.querySelector('#follower-' + pollId)
+let updateFollow = (evt, data, status, xhr) => {
+   let response = evt["detail"][0]
+   let pollId = response["pollId"]
+   let pollFollowElement = document.getElementById("poll-follow-" + pollId)
+   let pollFollowIcon = document.querySelector('#follower-' + pollId)
    pollFollowElement.innerHTML = response["content"]
    pollFollowIcon.classList = response["icon"];
-   console.log(pollFollowIcon.classList)
-   pollFollowLink = document.getElementById("poll-follow-path-" + pollId)
+   let pollFollowLink = document.getElementById("poll-follow-path-" + pollId)
    pollFollowLink.setAttribute('href', response["path"])
    pollFollowLink.setAttribute('data-method', response["method"])
-
 }
 
-function handleFollowError(evt, xhr, status, error) {
-   response = evt["detail"][0]
-   pollId = response["pollId"]
-   console.log(response)
-   pollFollowElement = document.getElementById("poll-follow-" + pollId)
-   console.log(pollFollowElement)
+let handleFollowError = (evt, xhr, status, error) => {
+   let response = evt["detail"][0]
+   let pollId = response["pollId"]
+   let pollFollowElement = document.getElementById("poll-follow-" + pollId)
    pollFollowElement.innerHTML = response["content"]
 }
