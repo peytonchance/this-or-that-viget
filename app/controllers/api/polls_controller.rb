c84fe57d3a7b5040b85e720845893bb7e52a51e7
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-07-24 13:40:30 -0600

    Implemented as_json serialization method

--- app/controllers/api/polls_controller.rb
+++ app/controllers/api/polls_controller.rb
@@ -1,42 +1,53 @@
 class Api::PollsController < Api::ApiController
   before_action :verify_user_id, only: :create
   before_action :verify_poll_id, only: :show
-  
+
   def index
     render json: {
-      poll: JSON.parse(Poll.recent.to_json)
+      poll: Poll.recent.as_json
       }, status: :accepted
   end
-  
+
   def create
-   
-  end
-  
-  def show
-    render json: {
-      poll: JSON.parse(Poll.find(params[:id]).to_json)
+    user = User.find(params[:user_id])
+    poll = user.polls.new(poll_params.merge(expired: false))
+    if poll.save 
+      render json: {
+      poll: JSON.parse(poll.to_json)
       }, status: :accepted
-  end
-  
-  private
-  def poll_params
-    params.require(:poll).permit(:title, :option_a_img, :option_b_img, :option_a_url, :option_b_url, :option_a, :option_b, expire: [:days, :hours, :mins])
-  end
-  
-  def verify_user_id
-    if !params[:user_id].present? || !User.find(params[:user_id]).present?
+    else
       render json: {
         "status": "error",
-        "message": "No user ID provided."
+        "message": poll.errors.full_messages
+        }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: {
+      poll: Poll.find(params[:id]).as_json
+      }, status: :accepted
+  end
+
+  private
+  def poll_params
+    params.require(:poll).permit(:title, :option_a_url, :option_b_url, :option_a, :option_b, expire: [:days, :hours, :mins])
+  end
+
+  def verify_user_id
+    if !params[:user_id].present? || !User.find_by(id: params[:user_id]).present?
+      render json: {
+        "status": "error",
+        "message": "Invalid User ID"
         }, status: :unauthorized
     end
   end
-  
+
   def verify_poll_id
-    if !params[:id].present? || !Poll.find(params[:id]).present?
+    if !params[:id].present? || !Poll.find_by(id: params[:id]).present?
       render json: {
         "status": "error",
-        "message": "No poll ID provided."
+        "message": "Invalid Poll ID"
         }, status: :unauthorized
     end
   end
