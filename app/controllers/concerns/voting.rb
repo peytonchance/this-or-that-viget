0e06898353648c57c2939d47e6f98196aa542af5
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-07-27 16:02:38 -0600

    Made suggested PR changes

---
+++ app/controllers/concerns/voting.rb
@@ -0,0 +1,42 @@
+module Voting
+  def create_vote_for(option)
+    poll = Poll.find(params[:poll_id])
+    vote = poll.votes.new(user: current_user, option: option)
+    if vote.save
+      render json: vote_success_json(poll).merge(
+        "pathA": poll_option_a_path(poll),
+        "pathB": poll_option_b_path(poll),
+        "method": "put"
+        ), status: :accepted
+    else
+      render json: vote_error_json, status: :unprocessable_entity
+    end
+  end
+
+  def update_vote_for(option)
+    poll = Poll.find(params[:poll_id])
+    vote = poll.votes.find_by(user_id: current_user.id)
+    if vote.update(option: option)
+      render json: vote_success_json(poll), status: :accepted
+    else
+      render json: vote_error_json, status: :unprocessable_entity
+    end
+  end
+
+
+  def vote_error_json
+    {
+      "status": "error",
+      "message": "Please try again"
+    }
+  end
+
+  def vote_success_json(poll)
+    {
+      "status": "success",
+      "poll": poll.id,
+      "optionA": poll.fraction_of_votes(0),
+      "optionB": poll.fraction_of_votes(1)
+    }
+  end
+end
--- app/controllers/option_as_controller.rb
+++ app/controllers/option_as_controller.rb
@@ -1,45 +1,11 @@
 class OptionAsController < ApplicationController
+  include Voting
+  
   def create
-    @poll = Poll.find(params[:poll_id])
-    @vote = @poll.votes.new(user: current_user, option: 0)
-    if @vote.save
-      render json: vote_success_json.merge(
-        "pathA": poll_option_a_path(@poll),
-        "pathB": poll_option_b_path(@poll),
-        "method": "put"
-        ), status: :accepted
-    else
-      render json: vote_error_json, status: :unprocessable_entity
-    end
+   create_vote_for(0)
   end
 
   def update
-    @poll = Poll.find(params[:poll_id])
-    @vote = @poll.votes.find_by(user_id: current_user.id)
-    if @vote.update(option: 0)
-      render json: vote_success_json, status: :accepted
-    else
-      render json: vote_error_json, status: :unprocessable_entity
-    end
-  end
-
-
-  private
-  def vote_error_json
-    @hash =  {
-        "status": "error",
-        "message": "Please try again"
-        }
-  end
-
-  def vote_success_json
-    @hash = {
-        "status": "success",
-        "poll": @poll.id,
-        "optionA": @poll.fraction_of_votes(0),
-        "optionB": @poll.fraction_of_votes(1),
-        "optionAText": @poll.option_a,
-        "optionBText": @poll.option_b
-      }
+    update_vote_for(0)
   end
 end
