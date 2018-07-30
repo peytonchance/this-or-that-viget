b000d5ba7cefb055c03a749226915b91303bbdfa
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-07-30 10:15:49 -0600

    Added follow API controller specs

---
+++ spec/controllers/api_follow_controller_spec.rb
@@ -0,0 +1,91 @@
+require "rails_helper"
+
+RSpec.describe Api::FollowsController, type: :controller do
+  let!(:token) {Rails.application.credentials.dig(:app_key)}
+
+  context "with existing polls, users, and follows" do
+    let!(:user) {create(:user)}
+    let!(:poll) {create(:poll)}
+    let!(:follow) {create(:follow, poll: poll, user: user)}
+
+    it 'responds with error without valid poll ID' do
+      get :show, params: {
+        token: token,
+        poll_id: 0,
+        user_id: user.id
+        }
+
+      expect(response.body).to eq({
+        status: "error",
+        message: "Invalid Poll ID"
+      }.to_json)
+      expect(response.status).to eq(422)
+    end
+
+    it 'responds with error without valid user ID' do
+      get :show, params: {
+        token: token,
+        poll_id: poll.id,
+        user_id: 0
+      }
+
+      expect(response.body).to eq({
+        status: "error",
+        message: "Invalid User ID"
+      }.to_json)
+      expect(response.status).to eq(422)
+    end
+
+    describe "creating a follow" do
+      it 'creates a new follow with valid params' do
+        follow_user = create(:user)
+        post :create, params: {
+          token: token,
+          poll_id: poll.id,
+          user_id: follow_user.id
+        }
+
+        expect(response.body).to eq({
+          status: "success",
+          follow: true
+        }.to_json)
+        expect(response.status).to eq(200)
+        expect(poll.follows.count).to eq(2)
+        expect(poll.follows.last.user).to eq(follow_user)
+      end
+    end
+
+    describe "showing if user is following poll" do
+      it 'shows true' do
+        get :show, params: {
+          token: token,
+          poll_id: poll.id,
+          user_id: user.id
+        }
+
+        expect(response.body).to eq({
+          status: "success",
+          follow: true
+        }.to_json)
+        expect(response.status).to eq(200)
+      end
+    end
+    
+    describe "unfollowing a poll" do
+      it 'unfollows the poll and returns false' do
+        delete :destroy, params: {
+          token: token,
+          poll_id: poll.id,
+          user_id: user.id
+        }
+
+        expect(response.body).to eq({
+          status: "success",
+          follow: false
+        }.to_json)
+        expect(response.status).to eq(200)
+        expect(poll.follows.count).to eq(0)
+      end
+    end
+  end
+end
