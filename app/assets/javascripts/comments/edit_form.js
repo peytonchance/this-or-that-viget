0eb9c9a5100c966d6ca2e7e20e229fc0ed39eaeb
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-08-01 14:26:21 -0600

    Progress halted to work on visitor voting

--- app/assets/javascripts/comments/edit_form.js
+++ app/assets/javascripts/comments/edit_form.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
     var commentEdits = document.getElementsByClassName('edit-comment')
-    console.log(commentEdits)
+    //console.log(commentEdits)
     if (commentEdits.length > 0) {
-      for (commentEdit in commentEdits) {
-         console.log(commentEdit)
+      for (i = 0; i < commentEdits.length; i++) {
+        commentEdits[i].addEventListener('click', handleEditComment)
       }
     }
 });
+
+function handleEditComment(e) {
+  console.log(e)
+}
