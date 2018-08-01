d2c38a90c9bdf406544ab779375cd49cb9bf2407
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-08-01 14:47:33 -0600

    Completed controllers

--- app/controllers/visitor_option_as_controller.rb
+++ app/controllers/visitor_option_as_controller.rb
@@ -1,2 +1,15 @@
 class VisitorOptionAsController < ApplicationController
+  include VisitorVoting
+  
+  def create
+   create_vote_for(0, request.remote_ip)
+  end
+
+  def update
+    update_vote_for(0, request.remote_ip)
+  end
+  
+  def destroy
+    destroy_vote_for(0, request.remote_ip)
+  end
 end
