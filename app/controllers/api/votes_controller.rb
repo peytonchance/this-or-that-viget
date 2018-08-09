983e0d32a64ab15be13f760397493bd4e53f9e66
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-08-09 10:34:01 -0600

    Added create action

--- app/controllers/api/votes_controller.rb
+++ app/controllers/api/votes_controller.rb
@@ -1,27 +1,37 @@
 class Api::VotesController < Api::ApiController
+  include ActionView::Helpers::TextHelper
+
+
   before_action :verify_poll_id
   before_action :verify_vote_type
+  before_action :verify_ip_or_user
+  before_action :verify_option, only: [:create, :update, :destroy]
 
   def create
+    if @type == "visitor"
+      vote = @poll.visitor_votes.new(ip_address: @ip, option: @option)
+    else
+      vote = @poll.votes.new(user: @user, option: @option)
+    end
+
+    if vote.save
+      render json: vote_success_json(@poll, @option), status: :ok
+    else
+      respond_with_error("Vote unable to be saved")
+    end
   end
 
   def show
     if @type == "visitor"
-      verify_ip_address
       vote = @poll.get_visitor_vote(@ip)
-      if vote.present?
-        respond_with_vote_given(vote.option)
-      else
-        respond_no_vote_given
-      end
     else
-      verify_user_id
       vote = @user.get_vote(@poll.id)
-      if vote.present?
-        respond_with_vote_given(vote.option)
-      else
-        respond_no_vote_given
-      end
+    end
+
+    if vote.present?
+      respond_with_vote_given(vote.option)
+    else
+      respond_no_vote_given
     end
   end
 
@@ -45,7 +55,26 @@ class Api::VotesController < Api::ApiController
       respond_with_error("Invalid voting type")
     end
   end
-  
+
+  def verify_ip_or_user
+    if @type == "visitor"
+      verify_ip_address
+    else
+      verify_user_id
+    end
+  end
+
+  def verify_option
+    @option = params[:option]
+    if !@option.present?
+      respond_with_error("No option given")
+    elsif @option != "1" && @option != "0"
+      respond_with_error("Invalid Option")
+    else
+      @option = @option.to_i
+    end
+  end
+
   def respond_no_vote_given
     render json: {
       status: "success",
@@ -54,7 +83,7 @@ class Api::VotesController < Api::ApiController
       vote: {}
     }, status: :ok
   end
-  
+
   def respond_with_vote_given(option)
     render json: {
       status: "success",
@@ -66,12 +95,15 @@ class Api::VotesController < Api::ApiController
     }, status: :ok
   end
 
-  def create_vote_for(type, option)
-  end
-
-  def update_vote_for(type, option)
-  end
-
-  def destroy_vote_for(type, option)
+  def vote_success_json(poll, option)
+    {
+      "status": "success",
+      "delete": false,
+      "poll": poll.id,
+      "optionA": poll.fraction_of_votes(0),
+      "optionB": poll.fraction_of_votes(1),
+      "option": option,
+      "count": pluralize(poll.vote_count, "vote")
+    }
   end
 end
